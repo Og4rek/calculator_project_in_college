b239a1b86466fb4c189d2d2e75ce878ff4243119
piti <piti@PIOTRYCZKO7E52>
2022-04-07 00:21:57 +0200

    Dodaj pliki projektów.

---
+++ calculator_ASL/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator_ASL
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            DevExpress.UserSkins.BonusSkins.Register();
+            DevExpress.Skins.SkinManager.EnableFormSkins();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new XtraForm1());
+        }
+    }
+}
---
+++ calculator_ASL/XtraForm1.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.XtraBars.Helpers;
+using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator_ASL
+{
+    public partial class XtraForm1 : DevExpress.XtraEditors.XtraForm
+    {
+        public XtraForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void XtraForm1_Load(object sender, EventArgs e)
+        {
+            SkinHelper.InitSkinPopupMenu(SkinsLink);
+        }
+    }
+}
